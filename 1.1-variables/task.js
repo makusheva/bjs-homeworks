20d1fd61139ad8a7e7edfc9e57fac7771369b211
makusheva <makushevaa1@gmail.com>
2020-03-23 16:45:29 +0200

    First homework

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -6,22 +6,27 @@
  */
 
 function averageMark(a, g, p) {
+	let algebra = a;
+	let geography = g;
+	let physics = p;
+	let averagePointCalculation = (a + g + p) / 3;
 	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+    return averagePointCalculation;
 }
 
 function sayHello(userName) {
-	
-    // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+	let myName = userName;
+	let message = `Привет, мир! Меня зовут ${userName}`;
+	    
+	return message;
 }
 
 function calculateFormula() {
+    let x = 2;
+	let y = 22;
+	let z = 0;
+	let result = x * y + 5 * z + x - 1;
+	
     
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+    return result;
 }

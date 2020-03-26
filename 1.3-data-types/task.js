1b7a113b40ffd7ca6c76435370996d9bab011fea
makusheva <makushevaa1@gmail.com>
2020-03-26 19:16:19 +0200

    Add HW 1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,36 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let date1 = new Date;
+        date2 = date;
+        year1 = date1.getFullYear();
+        year2 = date2.getFullYear();
+        month1 = date1.getMonth();
+        month2 = date2.getMonth();
+        loanBody = amount - contribution;
+        percentFractionalNumber = percent / 100 / 12;
+    if(month1===0){ 
+        month1++;
+        month2++;
+    }
+    let numberOfMonths; 
+    numberOfMonths = (year2 - year1) * 12 + (month2 - month1);
+
+    let monthlyFee = loanBody * (percentFractionalNumber + percentFractionalNumber / (Math.pow((1 + percentFractionalNumber), numberOfMonths) - 1))
+    let totalAmount = monthlyFee * numberOfMonths;
+    totalAmount = parseFloat(totalAmount.toFixed(2));
+    console.log(numberOfMonths);
+    console.log(loanBody);
+    console.log(monthlyFee);
+    return totalAmount;
 }
 
+
+
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+	if (name == '' || name == 'null' || name =='undefined') {
+		name = 'Аноним'
+	}
+	
+	greeting = `Привет, мир! Меня зовут ${name}`;
+	
+	return greeting;
 }

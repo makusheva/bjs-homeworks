fb3b12152f83c301606b6ffda1e926904ac1a8f3
makusheva <makushevaa1@gmail.com>
2020-04-04 20:01:05 +0300

    Add HW 2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,48 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+    let animalSound;
+
+    if (animal == undefined) {
+        animalSound = null    
+    } else {
+        animalSound = animal.sound;
+    }
+   
+    return animalSound
 }
 
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+function sum (marks) {
+    let total = 0;
+    for(let i = 0; i < marks.length; i++){
+        total += parseInt(marks[i]);
+    }
+    return total;
 }
 
-function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+function getAverageMark(marks){
+    let fractionalAverageMark = 0;
+   
+   if (marks.length == 0) {
+       return 0;
+   } else {
+    fractionalAverageMark = sum(marks) / marks.length
+   }
+   let averageMark = parseFloat(fractionalAverageMark.toFixed());
+    return averageMark;
+  }
+
+
+function checkBirthday(info) {
+   let now = Date.now();
+   let InfoDate = Date.parse(info);
+   let diff = now - InfoDate;
+   let age = diff / 31536000000;
+   let ageYears = Math.floor(age);
+   let verdict;
+    if (ageYears >= 18) {
+        verdict = true;
+    } else {
+        verdict = false;
+    }
+   
+    return verdict
 }
